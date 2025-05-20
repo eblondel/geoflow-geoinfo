5572b8adddf5d56911b2e662a50167ab36a00482
eblondel <emmanuel.blondel1@gmail.com>
2025-05-20 17:47:34 +0200

    additional grsf registers

--- registers/grsf_registers.R
+++ registers/grsf_registers.R
@@ -29,3 +29,32 @@ resource_register <- function(config){
 	out <- out[!is.na(out$code),]
 	return(out)
 }
+
+type_register <- function(config){
+  data = googledrive::drive_download(googledrive::drive_find("grsf_resource_placemarks.gpkg")$id, overwrite = TRUE)
+  sp = sf::st_read(data$name)
+  out <- data.frame(
+    code = sp$type,
+    uri = NA,
+    label = sp$type,
+    definition = NA,
+    stringsAsFactors = FALSE
+  )
+  out <- out[!is.na(out$code),]
+  return(out)
+}
+
+sdg_flag <- function(config){
+  data = googledrive::drive_download(googledrive::drive_find("grsf_resource_placemarks.gpkg")$id, overwrite = TRUE)
+  sp = sf::st_read(data$name)
+  out <- data.frame(
+    code = sp$sdg_flag,
+    uri = NA,
+    label = sp$sdg_flag,
+    definition = NA,
+    stringsAsFactors = FALSE
+  )
+  out <- out[!is.na(out$code),]
+  return(out)
+}
+
