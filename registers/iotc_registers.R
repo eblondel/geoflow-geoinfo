5ed5a268baa2daebc94d29885e54d7b2af784071
eblondel <emmanuel.blondel1@gmail.com>
2025-11-21 15:52:37 +0100

    update iotc register

--- registers/iotc_registers.R
+++ registers/iotc_registers.R
@@ -1,5 +1,5 @@
 iotc_codes_register <- function(config){
-  req <- readr::read_csv("C:/Users/artur/OneDrive/Documents/GitHub/geoflow-geoinfo/registers/cl_iotc_codes.csv", guess_max = 0)
+  req <- readr::read_csv("./registers/cl_iotc_codes.csv", guess_max = 0)
   out <- as.data.frame(req)[, c("code", "uri", "label", "definition")]
   out <- out[!is.na(out$code), ]
   return(out)
